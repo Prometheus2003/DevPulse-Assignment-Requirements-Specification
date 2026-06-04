b15503651aab0d5718124e0a3015799c5756d653
Prometheus2003 <samiulmahmud14@gmail.com>
2026-06-05 01:41:24 +0600

    send response

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,19 +1,24 @@
 import type { Request, Response } from "express"
 import { userService } from "./user.service"
+import sendResponse from "../../utils/sendResponse"
 
 
 const createUser = async (req: Request, res: Response) => {
     //const { name, email, password, age } = req.body
     try {
         const result = await userService.createUserIntoDB(req.body)
-        res.status(201).json({
-            message: "User created successfully",
-            data: result,
+        sendResponse(res, {
+            statusCode: 201,
+            success: true,
+            message: "User Created Successfully",
+            data: result
         })
     } catch (error: any) {
-        res.status(500).json({
+        sendResponse(res, {
+            statusCode: 500,
+            success: false,
             message: error.message,
-            error: error,
+            data: error
         })
     }
 }
